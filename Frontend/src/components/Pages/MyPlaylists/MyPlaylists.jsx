b715b07457fabcde5379c6cdd56401e31a94c0d4
Manpreet Singh <66904586+msingh17122000@users.noreply.github.com>
2025-03-10 22:09:18 +0530

    Music

--- Frontend/src/components/Pages/MyPlaylists/MyPlaylists.jsx
+++ Frontend/src/components/Pages/MyPlaylists/MyPlaylists.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 function MyPlaylists() {
+  const audioRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlayPause = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
   return (
     <div>
       My Playlists
+      <div>
+        <audio ref={audioRef} src="./assets/music/BalleBalle.mp3" />
+        <button onClick={togglePlayPause}>
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+      </div>
     </div>
   )
 }
