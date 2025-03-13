4f1a753222d4814e57d7e1c897300ddf01cbd6a0
Manpreet Singh <66904586+msingh17122000@users.noreply.github.com>
2025-03-13 15:20:19 +0530

    Bottom Navigation bar animation

--- Frontend/src/components/BottomBar/BottomBar.jsx
+++ Frontend/src/components/BottomBar/BottomBar.jsx
@@ -3,7 +3,7 @@ import "./BottomBar.css"
 
 import { FaHeart, FaPause, FaPlay } from "react-icons/fa";
 import { IoMusicalNotesSharp } from "react-icons/io5";
-import { IoIosPause, IoIosPlay, IoIosSearch } from "react-icons/io";
+import { IoIosArrowUp, IoIosPause, IoIosPlay, IoIosSearch } from "react-icons/io";
 import { LuHeart } from "react-icons/lu";
 import { Link, NavLink, useLocation, useParams } from 'react-router-dom';
 import { AnimatePresence, motion } from 'motion/react'
@@ -42,23 +42,33 @@ function BottomBar() {
   return (
     <motion.div className='bottom-bar-wrapper'>
       <motion.div className='bottom-bar'
-        animate={{ height: currentSong && 'auto' }}  // Expand when song plays
-        transition={{ type: "spring", stiffness: 100, duration: 0.1 }} // Smooth animation
+        // animate={{ height: currentSong && 'auto' }}  // Expand when song plays
+        // transition={{ type: "spring", stiffness: 100, duration: 0.1 }} // Smooth animation
+        animate={{ height: currentSong && showMusicController ? 'auto':'75px', }} // Expands first
+        style={{ overflow: 'hidden' }}
+        transition={{ type: "spring", stiffness: 120, duration: 0.3 }}
       >
         <AnimatePresence>
           {
             currentSong && showMusicController && (
               <motion.div
                 className='bottom-music-controller-wrapper'
-                initial={{ y: 100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                exit={{ y: 100, opacity: 0 }}
-
+                // initial={{ y: 100, opacity: 0 }}
+                // animate={{ y: 0, opacity: 1 }}
+                // exit={{ y: 100, opacity: 0 }}
+                initial={{scaleY:0}}
+                animate={{scaleY:1}}
+                exit={{scaleY:0}}
+                style={{height:showMusicController&&currentSong?'auto':0,padding:showMusicController&&currentSong?'10px':0,overflow:'hidden'}}
               >
+
                 {/* Song Info */}
                 <Link className='bottom-music-controller-music-data' to={'/now-playing'}>
-                  <div className='bottom-music-controller-music-name'>{currentSong.name}</div>
-                  <div className='bottom-music-controller-music-artist'>{currentSong.artist}</div>
+                  <motion.button className='bottom-music-controller-mode-button'><IoIosArrowUp size={20}/></motion.button>
+                  <div>
+                    <div className='bottom-music-controller-music-name'>{currentSong.name}</div>
+                    <div className='bottom-music-controller-music-artist'>{currentSong.artist}</div>
+                  </div>
                 </Link>
 
                 {/* Play/Pause Button */}
@@ -73,11 +83,11 @@ function BottomBar() {
             )
           }
 
-          {currentSong && showMusicController && <motion.hr className='separator' initial={{ width: 0 }} animate={{ width: '100%' }} exit={{ width: 0 }} />}
+          {/* {currentSong && showMusicController && <motion.hr className='separator' initial={{ width: 0 }} animate={{ width: '100%' }} exit={{ width: 0 }} />} */}
         </AnimatePresence>
 
         {/* Navigation */}
-        <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', padding: '15px 0px' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', padding: '15px 0px',alignItems:'flex-end' }}>
           <NavLink to='/favourites' className={({ isActive }) => `bottom-bar-navlink ${isActive ? 'active-bottom-link' : ''}`}><LuHeart size={20} /><div className='bottom-bar-title'>Favourites</div></NavLink>
           <div className='bottom-bar-separator' />
           <NavLink to='/playlists' className={({ isActive }) => `bottom-bar-navlink ${isActive ? 'active-bottom-link' : ''}`}><IoMusicalNotesSharp size={20} /><div className='bottom-bar-title'>Playlist</div></NavLink>
